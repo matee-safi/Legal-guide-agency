141eb96be751c446ec359b065f21c17e5f4e5742
Matee <matiullahsafibbc@gmail.com>
2023-09-07 18:34:50 +0430

    fix contact links

--- src/components/contact.js
+++ src/components/contact.js
@@ -9,11 +9,12 @@ const Contact = () => {
         <p className="mb-10">Reach out to us through our socials or come to our office, we'll get you set up in no time!</p>
         <h2 className="mb-3 text-xl font-bold">Contact Us</h2>
         <div className="mb-5 flex justify-center gap-5">
-          <a href="facebook.com"><StaticImage width={30} src="../../static/facebook.png" alt="facebook icon" /></a>
-          <a href="facebook.com"><StaticImage width={30} src="../../static/gmail.png" alt="gmail icon" /></a>
-          <a href="facebook.com"><StaticImage width={30} src="../../static/whatsapp.png" alt="whatsapp icon" /></a>
+          <a href="https://www.facebook.com/legalguide.edu" target="_blank"><StaticImage width={30} src="../../static/facebook.png" alt="facebook icon" /></a>
+          <a href="mailto:legalguide.edu@gmail.com" target="_blank"><StaticImage width={30} src="../../static/gmail.png" alt="gmail icon" /></a>
+          <a href="https://api.whatsapp.com/send?phone=0202200820" target="_blank"><StaticImage width={30} src="../../static/whatsapp.png" alt="whatsapp icon" /></a>
         </div>
-        <p><StaticImage src="../../static/location.png" alt="location icon" /> Shahre Naow, Haji Yaqoob Square</p>
+        <h2 className="mt-10 text-xl font-bold"><StaticImage src="../../static/location.png" alt="location icon" /> Our Location</h2>
+        <p>Shahre Naw, Haji Yaqoob Square, Beside Golden Star Hotel, Kabul Business Center, 5th Floor Office# 506 & 507, Kabul, Afghanistan</p>
       </div>
     </section>
    );
