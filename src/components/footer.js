76dd3127825862790f01c68a6695dca728e60c43
Matee <matiullahsafibbc@gmail.com>
2023-10-17 12:39:01 +0430

    update footer and nav

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Footer = () => {
   return ( 
-    <footer className="border-t text-center text-xs p-2 w-full">
+    <footer className="border-t text-center text-xs p-3 md:p-4 w-full">
       <p className="md:text-sm">© 2023 Legal Guide. All rights reserved.</p>
     </footer>
    );
